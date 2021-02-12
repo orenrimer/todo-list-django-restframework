11004fca0d82aef707a507699315b630a71ac12c
Oren Rimer <76216721+orenrimer@users.noreply.github.com>
2021-02-12 23:21:32 +0200

    Update admin.py

--- src/todo_list/api/admin.py
+++ src/todo_list/api/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_completed')
+    search_fields = ('title',)
+    readonly_fields = ('created_at',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Task,TasksAdmin)
